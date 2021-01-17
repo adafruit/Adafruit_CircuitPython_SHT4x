8f5e77acddf18bc511dc2ba2226d033b9bbaa73d
lady ada <limor@ladyada.net>
2021-01-17 12:39:26 -0500

    clean init

---
+++ adafruit_sht4x.py
@@ -0,0 +1,32 @@
+# SPDX-FileCopyrightText: 2017 Scott Shawcroft, written for Adafruit Industries
+# SPDX-FileCopyrightText: Copyright (c) 2021 ladyada for Adafruit
+#
+# SPDX-License-Identifier: MIT
+"""
+`adafruit_sht4x`
+================================================================================
+
+Python library for Sensirion SHT4x temperature and humidity sensors
+
+* Author(s): ladyada
+
+Implementation Notes
+--------------------
+
+**Hardware:**
+
+Python library for Sensirion SHT4x temperature and humidity sensors
+
+**Software and Dependencies:**
+
+* Adafruit CircuitPython firmware for the supported boards:
+  https://github.com/adafruit/circuitpython/releases
+
+* Adafruit's Bus Device library: https://github.com/adafruit/Adafruit_CircuitPython_BusDevice
+
+"""
+
+# imports
+
+__version__ = "0.0.0-auto.0"
+__repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_SHT4x.git"
---
+++ docs/examples.rst
@@ -0,0 +1,8 @@
+Simple test
+------------
+
+Ensure your device works with this simple test.
+
+.. literalinclude:: ../examples/sht4x_simpletest.py
+    :caption: examples/sht4x_simpletest.py
+    :linenos:
