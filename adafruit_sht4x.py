6bbb0ad55f3d197fcf3370e5fb1a7eec9df85aed
MkLHX <mickael.lehoux@greenponik.com>
2021-03-01 16:54:28 +0100

    change variable name

--- adafruit_sht4x.py
+++ adafruit_sht4x.py
@@ -90,8 +90,8 @@ class SHT4x:
 
     """
 
-    def __init__(self, i2c_bus, i2c_addr=_SHT4X_DEFAULT_ADDR):
-        self.i2c_device = i2c_device.I2CDevice(i2c_bus, i2c_addr)
+    def __init__(self, i2c_bus, address=_SHT4X_DEFAULT_ADDR):
+        self.i2c_device = i2c_device.I2CDevice(i2c_bus, address)
         self._buffer = bytearray(6)
         self.reset()
         self._mode = Mode.NOHEAT_HIGHPRECISION  # pylint: disable=no-member
