428794f15971f4199c8d5e05fa67f496a56ff3d2
Alec Delaney <tekktik@gmail.com>
2022-08-16 18:09:14 -0400

    Update version string

--- adafruit_sht4x.py
+++ adafruit_sht4x.py
@@ -33,7 +33,7 @@ import struct
 from adafruit_bus_device import i2c_device
 from micropython import const
 
-__version__ = "0.0.0-auto.0"
+__version__ = "0.0.0+auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_SHT4x.git"
 
 
